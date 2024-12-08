07047e70a08ad686b9352ab8c6bcca60b55f2c50
ashwitha1412 <23124004@srcas.ac.in>
2024-12-08 10:39:00 +0530

    push

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class one
+{ 
+ void cscs()
+ {
+  System.out.println("cscs");
+ }
+}
+class two extends one
+{ 
+ void dept()
+ {
+  System.out.println("dept");
+ }
+}
+class main1
+{
+ public static void main(String args[])
+ {
+  two c=new two();
+  g.cscs();
+  g.dept();
+ }
+}
